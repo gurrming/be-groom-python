4efe74faf63c3ba7f81b5c652e6932e5164d86ba
Yun Mina <dbsalsk2514@naver.com>
2026-02-10 09:55:00 +0900

    feat: python3으로 변경

--- run_collector.sh
+++ run_collector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /Users/anseungju/Desktop/dev_practice
 source venv/bin/activate
-python src/collectors/community_aggregator.py >> collector.log 2>&1
+python3 src/collectors/community_aggregator.py >> collector.log 2>&1
